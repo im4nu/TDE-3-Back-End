26fdefc9f8ca138e9a07f32705c5010331deb62f
manu <victorx811@gmail.com>
2025-05-12 17:34:01 -0300

    3rd time

--- config/db.php
+++ config/db.php
@@ -1,12 +1,13 @@
 <?php
 $host = '127.0.0.1';
-$dbname = 'locadora_2';
+$dbname = 'locadora';
 $user = 'root';
 $pass = 'nova_senha';
 
-$conn = new mysqli($host, $user, $pass, $dbname);
-
-if ($conn->connect_error) {
-    die("Erro na conexão: " . $conn->connect_error);
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro na conexão: " . $e->getMessage());
 }
 ?>
